e508e9ab044f21af30a937ea3baf86bcec5a46ee
varda <vardajais13@gmail.com>
2023-01-03 13:11:32 +0530

    first commit

---
+++ notes.js
@@ -0,0 +1,10 @@
+// Programming,computer is dumb.So set of instruction to tell the computer.
+// Ecmascript is a standard,it was to create different document on js ,talk about the same language.
+//Ecmascript can be interchangebly used as javascript.
+//Js is very liberal on what it allows.
+//Js is used to make websites interactively.
+//Js is executed in browser.
+//Node.js is used to run js outside the browser,Ryan Dahl.
+//Node js is runtime environment.
+//In html it can be used.
+//Repl.it can be used to run js
